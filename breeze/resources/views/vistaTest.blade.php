5becc91e2f399ec72daed5fdc603cf6d3ae1d19e
Marcos <exegeses@gmail.com>
2022-09-07 22:03:35 -0300

    middleware auth combinado con Blade+Bootstrap

---
+++ breeze/resources/views/vistaTest.blade.php
@@ -0,0 +1,24 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Vista Test</title>
+    <!-- CSS only -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-iYQeCzEYFbKjA/T2uDLTpkwGzCiq6soy8tYaI1GyVh/UjpbCx/TYkiZhlZB6+fzT" crossorigin="anonymous">
+</head>
+<body>
+    <header class="p-5 bg-warning">
+        <nav class="container lead">menu</nav>
+    </header>
+    <main class="container py-4">
+        <h1>Vista Test</h1>
+    </main>
+    <footer class="fixed-bottom p-5 bg-light text-center">
+        leyenda pie de página
+    </footer>
+
+</body>
+</html>
--- breeze/routes/web.php
+++ breeze/routes/web.php
@@ -21,4 +21,23 @@ Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth'])->name('dashboard');
 
+Route::get('/marcas', function ()
+{
+    return view('marcas');
+})->middleware(['auth'])->name('marcas');
+
+Route::get('/categorias', function ()
+{
+    return view('categorias');
+})->middleware(['auth'])->name('categorias');
+
+
+/* ruta sin usaar las vistas de breeze */
+Route::get('/testAuth', function ()
+{
+    return view('vistaTest');
+})->middleware(['auth']);
+
+
+
 require __DIR__.'/auth.php';
